0e60e7f4d60154b5a394c6608b8334ffdd5d647a
anirudhsingh05 <anirudhsingh05032001@gmail.com>
2024-05-02 08:03:13 +0530

    Completed Wordle Game Project

--- wordly.js
+++ wordly.js
@@ -1,27 +1,143 @@
-const letter = document.querySelectorAll('.scoreboard-letter');
-const loadingDiv = document.querySelector('.info-bar');
+const letters = document.querySelectorAll(".scoreboard-letter");
+const loadingDiv = document.querySelector(".info-bar");
+const ANSWER_LENGTH = 5;
+const ROUNDS = 6;
 
 async function init() {
+  let currentGuess = "";
+  let currentRow = 0;
+  let isLoading = true;
 
-    document.addEventListener('keydown', function handleKeyPress(event) {
-        const action = event.key;
-        console.log(action);
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+  const resObj = await res.json();
+  const word = resObj.word.toUpperCase();
+  const wordParts = word.split("");
+  let done = false;
+  setLoading(false);
+  isLoading = false;
 
-        if (action === 'Enter') {
-            commit();
-        } else if (action === 'Backspace') {
-            backSpace();
-        } else if (isLetter(action)) {
-            addLetter(action.toUpperCase());
-        } else {
-            // do Nothing
-        }
+  function addLetter(letter) {
+    if (currentGuess.length < ANSWER_LENGTH) {
+      currentGuess += letter;
+    } else {
+      currentGuess =
+        currentGuess.substring(0, currentGuess.length - 1) + letter;
+    }
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerHTML =
+      letter;
+  }
 
+  async function commit() {
+    if (currentGuess.length !== ANSWER_LENGTH) {
+      return;
+    }
+
+    isLoading = true;
+    setLoading(true);
+    const res = await fetch("https://words.dev-apis.com/validate-word", {
+      method: "POST",
+      body: JSON.stringify({ word: currentGuess }),
     });
+
+    const resObj = await res.json();
+    const { validWord } = resObj;
+
+    isLoading = false;
+    setLoading(false);
+
+    if (!validWord) {
+      markInvalidWord();
+      return;
+    }
+
+    const guessParts = currentGuess.split("");
+    const map = makeMap(wordParts);
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+        map[guessParts[i]]--;
+      }
+    }
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        // do nothing we already did
+      } else if (map[guessParts[i]] > 0 && wordParts.includes(guessParts[i])) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("close");
+        map[guessParts[i]]--;
+      } else {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
+      }
+    }
+
+    currentRow++;
+
+    if (currentGuess === word) {
+      alert("You Win");
+      document.querySelector(".brand").classList.add("winner");
+      done = true;
+      return;
+    } else if (currentRow === ROUNDS) {
+      alert(`You lose, The word was ${word}`);
+      done = true;
+    }
+    currentGuess = "";
+  }
+
+  function backSpace() {
+    currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerHTML = "";
+  }
+
+  function markInvalidWord() {
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      letters[currentRow * ANSWER_LENGTH + i].classList.remove("invalid");
+
+      setTimeout(function () {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("invalid");
+      }, 10);
+    }
+  }
+
+  document.addEventListener("keydown", function handleKeyPress(event) {
+    if (done || isLoading) {
+      return;
+    }
+
+    const action = event.key;
+
+    if (action === "Enter") {
+      commit();
+    } else if (action === "Backspace") {
+      backSpace();
+    } else if (isLetter(action)) {
+      addLetter(action.toUpperCase());
+    } else {
+      // do Nothing
+    }
+  });
 }
 
 function isLetter(letter) {
-    return /^[a-zA-z]$/.test(letter);
+  return /^[a-zA-z]$/.test(letter);
+}
+
+function setLoading(isLoading) {
+  loadingDiv.classList.toggle("show", isLoading);
+}
+
+function makeMap(array) {
+  const obj = {};
+  for (let i = 0; i < array.length; i++) {
+    const letter = array[i];
+    if (obj[letter]) {
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+  return obj;
 }
 
 init();
